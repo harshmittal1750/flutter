6cfe445b7366677bdf3f27c3059ecb53689facf3
harshmittal1750 <harshmittal1750@gmail.com>
2020-12-02 17:56:00 +0530

    2nd commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World'),));
-}
+  runApp(
+    MaterialApp(
 
+      home: Scaffold(
+        backgroundColor:Colors.blueGrey,
+
+        appBar: AppBar(
+          title: Text('i m rich'),
+          backgroundColor:Colors.amber[900],
+
+        ),
+        body: Image(image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),)
+      ),
+    ),
+  );
+}
